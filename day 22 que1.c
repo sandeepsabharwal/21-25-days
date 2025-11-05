65ee5c9febc0f840f28c01d3919b485c74df0e56
Sandeep Sabharwal <sabharwalsandeep989@gmail.com>
2025-11-05 17:36:19 +0530

    Create day 22 que1.c

---
+++ day 22 que1.c
@@ -0,0 +1,29 @@
+# Write a program to check if a number is a strong number.
+
+#include <stdio.h>
+
+int factorial(int n) {
+    int fact = 1;
+    for (int i = 1; i <= n; i++)
+        fact *= i;
+    return fact;
+}
+
+int main() {
+    int num, original, sum = 0;
+    scanf("%d", &num);
+    original = num;
+
+    while (num != 0) {
+        int digit = num % 10;
+        sum += factorial(digit);
+        num /= 10;
+    }
+
+    if (sum == original)
+        printf("%d is a Strong number.\n", original);
+    else
+        printf("%d is not a Strong number.\n", original);
+
+    return 0;
+}

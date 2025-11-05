9eb8e7e6e6050043cc100f92981a075c0ce361d6
Sandeep Sabharwal <sabharwalsandeep989@gmail.com>
2025-11-05 17:38:54 +0530

    Create day23 que1.c

---
+++ day23 que1.c
@@ -0,0 +1,20 @@
+# Write a program to find the sum of the series: 2/3 + 4/7 + 6/11 + 8/15 + ... up to n terms.
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    float sum = 0.0;
+    scanf("%d", &n);
+
+    int numerator = 2, denominator = 3;
+
+    for (int i = 1; i <= n; i++) {
+        sum += (float)numerator / denominator;
+        numerator += 2;
+        denominator += 4;
+    }
+
+    printf("Sum of the series up to %d terms: %.4f\n", n, sum);
+    return 0;
+}
